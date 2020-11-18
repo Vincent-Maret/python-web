2de198faf98265324888671c50dad757bd82b31d
papaya-industry <maretvincent@protonmail.com>
2020-11-18 12:19:44 +0100

    add error il no csv found and add non recursive for os.walk

--- streamlit/loadData.py
+++ streamlit/loadData.py
@@ -4,18 +4,21 @@ import pandas as pd
 
 def getCsvPaths(path):
     assert os.path.exists(
-        path), "File not found at path: "+str(path)
+        path), "Directory not found: "+str(path)
 
     csvPaths = []
     for (root, dirNames, fileNames) in os.walk(path):
         for fileName in fileNames:
             if fileName.endswith('.csv'):
                 csvPaths.append(os.path.join(root, fileName))
+        break
 
     return csvPaths
 
 
 def loadCsvDataFrames(paths):
+    assert len(paths) > 0, 'No CSV file found in givent directory.'
+
     data = []
     for path in paths:
         data.append(pd.read_csv(path))
