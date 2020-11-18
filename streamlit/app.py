62b97d656b72cf1230296a3f5c8a6d63523a2984
papaya-industry <maretvincent@protonmail.com>
2020-11-18 12:45:46 +0100

    add data car

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,14 +6,16 @@ import seaborn as sns
 
 import loadData as ld
 
+data = []
+
 st.title("TP Vincent Maret Streamlit")
 st.header("Simple Header")
 
 
-userInput = st.text_input('Path to your data folder', './data/')
+userInput = st.text_input('Path to your data directory or csv file', './data/')
 
 if userInput:
     if st.button("Load data"):
         csvPaths = ld.getCsvPaths(userInput)
-        data = ld.loadCsvDataFrames(csvPaths)
+        data = ld.loadCsvDataFrames(csvPaths, data)
         st.dataframe(data[0].head())
